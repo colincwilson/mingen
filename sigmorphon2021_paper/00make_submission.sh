11945a2e7153254f454700bd46b2306ca1636a06
Colin Wilson <colin.chris.wilson@gmail.com>
2021-07-15 12:22:23 -0700

    paper submission commit

---
+++ sigmorphon2021_paper/00make_submission.sh
@@ -0,0 +1,7 @@
+cp wilsonlisigmorphon2021.tex wilsonlisigmorphon2021/
+cp wilsonlisigmorphon2021.bib wilsonlisigmorphon2021/
+cp wilsonlisigmorphon2021extra.bib wilsonlisigmorphon2021/
+cp anthology.bib wilsonlisigmorphon2021/
+cp wilsonlisigmorphon2021.pdf wilsonlisigmorphon2021/
+
+# then zip and send
